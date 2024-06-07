78173afae6e5e6df295b106c96994d7ac87f0c98
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-07 13:17:52 +0200

    aggiunta array e evento click

--- js/myjs.js
+++ js/myjs.js
@@ -1,2 +1,9 @@
 let contMessaggio = document.querySelector('.contMessaggio');
 let buttn = document.querySelector('button');
+
+// array contenenti tutte le email
+let arrEmail = ['tommaso.galistu@gmail.it', 'tommasogalistu@gmail.con', 'tommasogallo@gmail.com', 'tommasogalistu92@gmail.com']
+
+buttn.addEventListener('click', () =>{
+    console.log('dentro')
+})
