f40ab51c6bdbaf234defee7d7a851dbb8f4624c9
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-07 14:07:59 +0200

    modifiche a primo esercizio FINE ESERCIZIO

--- js/myjs.js
+++ js/myjs.js
@@ -1,6 +1,6 @@
 let contMessaggio = document.querySelector('.contMessaggio');
 let buttn = document.querySelector('button');
-let textInput = document.querySelector('input').value;
+let textInput = document.querySelector('input');
 
 // array contenenti tutte le email
 let arrEmail = ['tommaso.galistu@gmail.it', 'tommasogalistu@gmail.con', 'tommasogallo@gmail.com', 'tommasogalistu92@gmail.com']
@@ -14,18 +14,17 @@ buttn.addEventListener('click', () =>{
     for (let i = 0; i < arrEmail.length; i++) {
         
         
-        if(arrEmail[i] === textInput && click === 0){
+        if(arrEmail[i] === textInput.value){
             let messPositivo = document.createElement('p')
             messPositivo.classList.add('messaggioPositivo')
             messPositivo.append('La tua email è presente nel database')
             contMessaggio.append(messPositivo)
-            click++
-        } else if(i === arrEmail.length - 1 && click === 0){
+            
+        } else if(i === arrEmail.length - 1){
             let messNegativo = document.createElement('p')
             messNegativo.classList.add('messaggioNegativo')
             messNegativo.append('Valori non validi')
             contMessaggio.append(messNegativo)
-            click++
         } 
         
         
